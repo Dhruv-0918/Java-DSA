d19ed21d298a698fe67bab1dc2efd80e7a07d2ec
Dhruv-0918 <dhruv.chourasiya18@gmail.com>
2025-11-21 20:05:37 +0530

    Lecture 4

---
+++ src/Lecture_4/string.java
@@ -0,0 +1,64 @@
+package Lecture_4;
+
+class lec4 {
+    public static void main(String[] args) {
+        int arr[] = new int[256];
+
+        // **To remove duplicate element
+
+//        String str = "dhruvvvv";
+//        String newStr="";
+//        for(char ch: str.toCharArray()){
+//            if(newStr.indexOf(ch)==-1){
+//                newStr+=ch;
+//            }
+//        }
+//        System.out.println(newStr);
+
+        //*****To Know Frequency
+
+//        String str = "Java is a programing language";
+//        for(int i=0;i<str.length();i++){
+//            arr[str.charAt(i)]++;
+//        }
+//           for(int i=0;i<256;i++){
+//                 if(arr[i]>0){
+//                     System.out.println((char)(i)+":"+ arr[i]);
+
+        //**To know Biggest word
+//        String str = "Java is a Programing language";
+//        String ch[] = str.split(" ");
+//        String Large = "";
+//        for (String val : ch) {
+//            if (val.length() > Large.length()) {
+//                Large = val;
+//            }
+//
+//        }
+//        System.out.println(Large);
+
+
+        //**To Know it is Anagram or not
+
+//        int arr1[] = new int[256];
+//        int arr2[] = new int[256];
+//
+//        String str0 = "hello";
+//        String str2 = "llohhjj";
+//
+//        for (int i = 0; i < str0.length(); i++) {
+//            arr[str0.charAt(i)]++;
+//
+//            for (int k = 0; i < str2.length(); i++) {
+//                arr[str2.charAt(k)]++;
+//            }
+//            for(int l=0;l<256;l++){
+//                if(arr[l]!=arr2[l]){
+//                    System.out.println("false");
+//                    return;
+//                }
+//            }
+//            System.out.println("True");
+//        }
+    }
+}
