e4b9e13838d7fa85c4dc5e65b284ba1dc5e0b8d9
Dhruv-0918 <dhruv.chourasiya18@gmail.com>
2025-11-24 11:08:05 +0530

    Lecture 7

---
+++ src/Lecture_7/bankaccount.java
@@ -0,0 +1,22 @@
+package Lecture_7;
+
+class Rectangle{
+    int length;
+    int breath;
+   void area(int length,int breath){
+       double a=length*breath;
+       System.out.println(a);
+    }
+    void parameter(int length,int breath){
+       int parameter=2*length+breath;
+        System.out.println(parameter);
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+
+         Rectangle display = new Rectangle();
+
+            }
+}
---
+++ src/Lecture_7/constructor.java
@@ -0,0 +1,23 @@
+package Lecture_7;
+
+class Carr{
+    int a;
+    String name;
+    Carr( int age,String name){
+        this.a=age;
+        this.name=name;
+        System.out.println("Name:"+" "+name);
+        System.out.println("a:"+" "+age);
+    }
+}
+class Lect_7{
+    public static void main(String[] args){
+
+    Carr s1=new Carr(18,"dhruv");
+        Carr s2=new Carr(19,"dhruv");
+        Carr s3=new Carr(21,"");
+
+
+
+    }
+}
